1f100c1fc2bfff6acdd3685b1c4610cf3352d40b
John McIntosh II <jmci10@hotmail.com>
2016-08-16 18:10:57 -0400

    add displayForTeamA method

--- app/src/main/java/com/ver2point0/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/ver2point0/android/courtcounter/MainActivity.java
@@ -1,7 +1,8 @@
 package com.ver2point0.android.courtcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamA(8);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
     }
 }
