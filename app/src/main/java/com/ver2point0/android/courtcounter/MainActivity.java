90d9531cd1c7a859094e04f24f3986de63e431d1
John McIntosh II <jmci10@hotmail.com>
2016-08-17 19:22:18 -0400

    add onClick methods to point buttons

--- app/src/main/java/com/ver2point0/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/ver2point0/android/courtcounter/MainActivity.java
@@ -2,6 +2,7 @@ package com.ver2point0.android.courtcounter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,7 +11,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(8);
+    }
+
+    /**
+     * Add 3 points to Team A score
+     */
+    public void addThreePoints(View view) {
+        displayForTeamA(3);
+    }
+
+    /**
+     * Add 2 points to Team A score
+     */
+    public void addTwoPoints(View view) {
+        displayForTeamA(2);
+    }
+
+    /**
+     * Add 1 point to Team A score
+     */
+    public void addOnePoint(View view) {
+        displayForTeamA(1);
     }
 
     /**
